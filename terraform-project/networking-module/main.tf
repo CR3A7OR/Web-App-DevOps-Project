bf48611e6a7b0a332b64c8b8ca80ec55c7585133
CR3A7OR <67477209+CR3A7OR@users.noreply.github.com>
2024-02-18 13:51:31 +0000

    Adds Assignment 4 to roster

---
+++ terraform-project/networking-module/main.tf
@@ -0,0 +1,68 @@
+
+# Create the Azure Resource Group for networking resources
+resource "azurerm_resource_group" "networking" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Define the Virtual Network (VNet) for the AKS cluster
+resource "azurerm_virtual_network" "aks-vnet" {
+  name                = "aks-vnet"
+  address_space       = var.vnet_address_space
+  location            = azurerm_resource_group.networking.location
+  resource_group_name = azurerm_resource_group.networking.name
+}
+
+# Define subnets within the VNet for control plane and worker nodes
+resource "azurerm_subnet" "control_plane_subnet" {
+  name                 = "control_plane_subnet"
+  resource_group_name  = azurerm_resource_group.networking.name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+ 
+resource "azurerm_subnet" "worker_node_subnet" {
+  name                 = "worker_node_subnet"
+  resource_group_name  = azurerm_resource_group.networking.name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+}
+
+# Define Network Security Group (NSG) for the AKS subnet
+resource "azurerm_network_security_group" "aks_nsg" {
+  name                = "aks-nsg"
+  location            = azurerm_resource_group.networking.location
+  resource_group_name = azurerm_resource_group.networking.name
+
+}
+
+resource "azurerm_network_security_rule" "kube_apiserver-rule" {
+  name                        = "kube_apiserver-rule"
+  priority                    = 1001
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "PUBLIC IP"  # Replace with your public IP or IP range
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.networking.name
+  network_security_group_name = azurerm_network_security_group.aks_nsg.name
+}
+
+# Allow inbound traffic for SSH (TCP/22)
+resource "azurerm_network_security_rule" "ssh-rule" {
+  name                        = "ssh-rule"
+  priority                    = 1002
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "PUBLIC IP" # Replace with your public IP or IP range
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.networking.name
+  network_security_group_name = azurerm_network_security_group.aks_nsg.name
+}
+
